c5a489672aacd3f56d3e9f56d6b91c39761cdc40
hailemichael121 <yihunaashe@gmail.com>
2024-03-29 16:12:41 +0300

    created achivment button in teh profile box

--- src/components/top/ProfileBox.tsx
+++ src/components/top/ProfileBox.tsx
@@ -1,37 +1,52 @@
-import { Box, VStack } from "@chakra-ui/react";
+import { Box, Button, HStack, VStack, useColorMode } from "@chakra-ui/react";
 import { ProfileText } from "..";
 import ProfilePhoto from "./ProfilePhoto";
 
 const ProfileBox = () => {
+  const { colorMode } = useColorMode();
+
   return (
     <VStack
-      boxShadow=" 10px 20px 20px 20px rgba(0, 0, 0, 0.2)"
+      margin={0}
+      backgroundColor="#ced8e4"
+      display="flex"
+      placeItems="start"
+      minWidth="60vh"
+      minHeight="70vh"
+      boxShadow="inset 0px 20px 20px 20px rgba(0, 0, 0, 0.2)"
+      _hover={{
+        backgroundcolor: "rgb(238,174,202)",
+        background:
+          "radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%)",
+        transform: "scale(1.1)",
+      }}
+      transition={"1.9s"}
       mr={150}
       ml={150}
       mb={100}
       mt={70}
     >
-      <Box
-        margin={0}
-        backgroundColor="#ced8e4"
-        display="flex"
-        placeItems="center"
-        minWidth="60vh"
-        minHeight="70vh"
-        boxShadow="inset 0px 20px 20px 20px rgba(0, 0, 0, 0.2)"
-        _hover={{
-          backgroundcolor: "rgb(238,174,202)",
-          background:
-            "radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%)",
-          transform: "scale(1.1)",
-        }}
-        transition={"1.9s"}
-      >
-        <ProfileText />
+      <HStack>
+        <Box>
+          <ProfileText />
+        </Box>
         <Box>
           <ProfilePhoto />
         </Box>
-      </Box>
+      </HStack>
+      <Button
+        ml={40}
+        mb={20}
+        mt={20}
+        backgroundColor="gray.50"
+        color={colorMode === "dark" ? "AppWorkspace" : "black"}
+        fontFamily="monospace"
+        fontWeight="500"
+        fontSize="x-large"
+        width="350px"
+      >
+        Achiements 🏆 →
+      </Button>
     </VStack>
   );
 };
