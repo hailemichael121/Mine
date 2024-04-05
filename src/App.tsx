632ac5f3089d432b6f3e8f49aea9a706d6e42e60
hailemichael121 <yihunaashe@gmail.com>
2024-04-05 21:34:23 +0300

    nothing changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/Routes";
+import "./App.css";
+
+import "./fonts/Arenq.otf";
+import "./fonts/Beckman-FREE.otf";
+import "./fonts/Mova.otf";
+import "./fonts/Mova.ttf";
 
 function App() {
   return <RouterProvider router={router} />;
