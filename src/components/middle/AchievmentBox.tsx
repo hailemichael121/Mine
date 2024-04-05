0a1e79092479c112035cd6ec54b98db246e6eac9
hailemichael121 <yihunaashe@gmail.com>
2024-04-05 21:36:26 +0300

    noting new

--- src/components/middle/AchievmentBox.tsx
+++ src/components/middle/AchievmentBox.tsx
@@ -3,14 +3,7 @@ import { Box } from "@chakra-ui/react";
 const AchievmentBox = () => {
   return (
     <>
-      <Box
-        margin={60}
-        backgroundColor="white"
-        display={"flex"}
-        placeItems={"center"}
-        minWidth={"10vh"}
-        minHeight={"10vh"}
-      ></Box>
+      <Box></Box>
     </>
   );
 };
