04af1c7e3d2e37605f20399dd44e92172347a3fd
hailemichael121 <yihunaashe@gmail.com>
2025-01-03 03:53:10 +0300

    refactor GitHubActivity component to improve layout, add tooltips, and enhance repository data structure

--- src/components/shared/GitHubActivity.tsx
+++ src/components/shared/GitHubActivity.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import GitHubCalendar from "react-github-calendar";
+import { Tooltip } from "@chakra-ui/react";
+
 import axios from "axios";
 import {
   Box,
@@ -13,15 +15,19 @@ import {
   Divider,
   Button,
   ButtonGroup,
+  VStack,
+  Link,
+  useBreakpointValue,
 } from "@chakra-ui/react";
 import { FaStar, FaGithub } from "react-icons/fa";
 
 const GitHubActivity: React.FC = () => {
   const [publicRepoCount, setPublicRepoCount] = useState(0);
   const [starCount, setStarCount] = useState(0);
-  const [repos, setRepos] = useState<string[]>([]);
+  const [repos, setRepos] = useState<{ name: string; url: string }[]>([]);
   const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
   const { colorMode } = useColorMode();
+  const isMobile = useBreakpointValue({ base: true, md: false });
 
   useEffect(() => {
     const fetchGitHubData = async () => {
@@ -42,10 +48,13 @@ const GitHubActivity: React.FC = () => {
         );
         setStarCount(stars);
 
-        const repoNames = reposResponse.data
+        const repoData = reposResponse.data
           .slice(0, 5)
-          .map((repo: { name: string }) => repo.name);
-        setRepos(repoNames);
+          .map((repo: { name: string; html_url: string }) => ({
+            name: repo.name,
+            url: repo.html_url,
+          }));
+        setRepos(repoData);
       } catch (error) {
         console.error("Error fetching GitHub data:", error);
       }
@@ -71,72 +80,106 @@ const GitHubActivity: React.FC = () => {
       borderRadius="md"
       boxShadow="lg"
     >
-      <Heading as="h1" size="40px" mt={4} mb={2}>
-        {" "}
-        GitHub Activity:{" "}
-      </Heading>
-      <Box display="flex" justifyContent="center" mb={4}>
-        <ButtonGroup size="sm" isAttached variant="outline">
-          {Array.from(
-            { length: 5 },
-            (_, i) => new Date().getFullYear() - i
-          ).map((year) => (
-            <Button
-              key={year}
-              onClick={() => handleYearChange(year)}
-              isActive={selectedYear === year}
-              _active={{
-                bg: "teal.500",
-                color: "white",
-              }}
-              _hover={{
-                bg: "teal.300",
-                color: "white",
-              }}
-              transition="all 0.3s"
-            >
-              {year}
-            </Button>
-          ))}
-        </ButtonGroup>
-      </Box>
-      <Box display="flex" justifyContent="center" mb={4}>
-        <GitHubCalendar username="hailemichael121" year={selectedYear} />
-      </Box>
-
-      <Flex justifyContent="space-between" alignItems="center" mt={4}>
-        <Flex alignItems="center">
-          <Icon as={FaGithub} w={6} h={6} mr={2} />
-          <Text fontSize="lg">Public Repositories: {publicRepoCount}</Text>
-        </Flex>
-        <Flex alignItems="center">
-          <Icon as={FaStar} w={6} h={6} mr={2} />
-          <Text fontSize="lg">Stars: {starCount}</Text>
-        </Flex>
-      </Flex>
-
-      <Heading as="h6" size="md" mt={6} mb={2}>
-        Top 5 Repositories:
-      </Heading>
-      <List spacing={2} mt={4}>
-        <Box position="relative" pl={4}>
-          <Divider
-            orientation="vertical"
-            position="absolute"
-            left="10px"
-            height="100%"
-            borderColor={colorMode === "light" ? "gray.400" : "gray.600"}
-          />
-          {repos.map((repo, index) => (
-            <ListItem key={index} pl={4} position="relative">
-              <Flex alignItems="center">
-                <Icon as={FaGithub} w={5} h={5} mr={2} />
-                <Text>{repo}</Text>
-              </Flex>
-            </ListItem>
-          ))}
+      <VStack>
+        <Box>
+          <Heading as="h1" size="lg" mt={4} mb={2} ml={6} textAlign="left">
+            GitHub Activity
+          </Heading>
+          <Box display="flex" justifyContent="center" mb={4}>
+            <ButtonGroup size="sm" isAttached variant="outline">
+              {Array.from(
+                { length: 5 },
+                (_, i) => new Date().getFullYear() - i
+              ).map((year) => (
+                <Button
+                  key={year}
+                  onClick={() => handleYearChange(year)}
+                  isActive={selectedYear === year}
+                  _active={{
+                    bg: "teal.500",
+                    color: "white",
+                  }}
+                  _hover={{
+                    bg: "teal.300",
+                    color: "white",
+                  }}
+                  transition="all 0.3s"
+                >
+                  {year}
+                </Button>
+              ))}
+            </ButtonGroup>
+          </Box>
+          <Box display="flex" justifyContent="center" mb={4} overflowX="auto">
+            <Box>
+              <GitHubCalendar
+                blockSize={isMobile ? 3 : 12}
+                blockRadius={isMobile ? 5 : 12}
+                username="hailemichael121"
+                year={selectedYear}
+                renderBlock={(block, activity) => (
+                  <Tooltip
+                    label={`${activity.count} activities on ${activity.date}`}
+                  >
+                    {block}
+                  </Tooltip>
+                )}
+                renderColorLegend={(block, level) => (
+                  <Tooltip label={`Level: ${level}`}>{block}</Tooltip>
+                )}
+              />
+            </Box>
+          </Box>
         </Box>
-      </List>
+
+        <Flex
+          justifyContent="space-evenly"
+          alignItems="center"
+          mt={4}
+          width={"100%"}
+          flexDirection={{ base: "column", md: "row" }}
+        >
+          <Flex direction="column" alignItems="center" mb={{ base: 2, md: 0 }}>
+            <Flex>
+              {" "}
+              <Icon as={FaGithub} w={6} h={6} mr={2} />
+              <Text fontSize="lg">Public Repositories: {publicRepoCount}</Text>
+            </Flex>
+            <Flex direction="column" alignItems={"flex-start"} pt={4}>
+              <Heading as="h6" size="md" mt={6} mb={2}>
+                Top 5 Repositories:
+              </Heading>
+              <List spacing={2} mt={4}>
+                <Box position="relative" pl={4}>
+                  <Divider
+                    orientation="vertical"
+                    position="absolute"
+                    left="10px"
+                    height="100%"
+                    borderColor={
+                      colorMode === "light" ? "gray.400" : "gray.600"
+                    }
+                  />
+                  {repos.map((repo, index) => (
+                    <ListItem key={index} pl={4} position="relative">
+                      <Flex alignItems="center" p={2}>
+                        <Icon as={FaGithub} w={5} h={5} mr={2} />
+                        <Link href={repo.url} isExternal>
+                          {repo.name}
+                        </Link>
+                      </Flex>
+                    </ListItem>
+                  ))}
+                </Box>
+              </List>
+            </Flex>
+          </Flex>
+          <Flex alignItems="flex-start" mb={{ base: 2, md: 0 }}>
+            <Icon as={FaStar} w={6} h={6} mr={2} />
+            <Text fontSize="lg">Stars: {starCount}</Text>
+          </Flex>
+        </Flex>
+      </VStack>
     </Box>
   );
 };
