540dba68c2800b19c75129b84b1173d2197eef5e
hailemichael121 <yihunaashe@gmail.com>
2025-03-11 11:51:03 +0300

    refactor ProjectSecHeader component to add animations and improve layout

--- src/components/projectsSec/ProjectSecHeader.tsx
+++ src/components/projectsSec/ProjectSecHeader.tsx
@@ -1,24 +1,37 @@
 import { Heading, HStack } from "@chakra-ui/react";
 import React from "react";
 import { GiPapers } from "react-icons/gi";
+import { motion } from "framer-motion";
 
 const ProjectSecHeader: React.FC = () => {
   return (
     <HStack justifyContent={"flex-start"}>
-      <Heading
-        className="project-sec-header"
-        alignItems="center"
-        fontSize={"50px"}
-        pb={10}
+      <motion.div
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
       >
-        Top Projects
-      </Heading>
-      <GiPapers
-        style={{
-          marginTop: "-50px",
-        }}
-        fontSize={"25px"}
-      />
+        <Heading
+          className="project-sec-header"
+          alignItems="center"
+          fontSize={"50px"}
+          pb={10}
+        >
+          Top Projects
+        </Heading>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8, delay: 0.2 }}
+      >
+        <GiPapers
+          style={{
+            marginTop: "-50px",
+          }}
+          fontSize={"25px"}
+        />
+      </motion.div>
     </HStack>
   );
 };
