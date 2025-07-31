e7654148a24241b50275a2a53a8bfb2462cbbb38
Haile_Michael <121032039+hailemichael121@users.noreply.github.com>
2025-07-31 00:30:41 -0700

    Update Resume.tsx

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -14,10 +14,14 @@ const Resume: React.FC = () => {
       <ResumeSecHeader />
       <SimpleGrid columns={[1, null, 2]} spacing="20px">
         <ResumeSecWorkExperience />
-       <Flex gap="20px">
-    <Box flex={1}><ResumeSecEducation /></Box>
-    <Box flex={1}><ResumeSecSkills /></Box>
-  </Flex>
+      <Flex direction={{ base: 'column', md: 'row' }} gap="20px" align="start">
+  <Box flex={1}>
+    <ResumeSecEducation />
+  </Box>
+  <Box flex={1}>
+    <ResumeSecSkills />
+  </Box>
+</Flex>
       </SimpleGrid>
       <Box mt={16}>
         {" "}
